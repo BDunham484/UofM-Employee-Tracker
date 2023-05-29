01778bf1986ea4794d3ce66041ec2fe8d9a95ef0
ThereWolf <bdunham484@gmail.com>
2023-05-29 17:00:46 -0500

    schema.sql updates

--- db/schema.sql
+++ db/schema.sql
@@ -25,8 +25,8 @@ CREATE TABLE employee (
     role_id INT,
     FOREIGN KEY (role_id)
     REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT
+    ON DELETE CASCADE,
+    manager_id INT,
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
